333bcc6719c7343bda05d28ca74c8edb5a82759c
the349 <josephmalone25@gmail.com>
2017-05-31 17:11:43 -0400

    Update dear.js

--- dear.js
+++ dear.js
@@ -1,4 +1,4 @@
-dcoument.write("<p>powered by dearjs <img src="image.png"></p>");
+//dcoument.write("<p>powered by dearjs <img src="image.png"></p>");
 function startdear(type) {
   if(type == "prompt") {
     document.write("<div id='dearjs-prompt-div' style='background-color: #C4CFCE; width: 60%; postiton: absolute; left: 0px;'>"+prompt+"</div>");
