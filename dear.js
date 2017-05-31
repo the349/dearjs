310c475e55d46f9c0fbf7fcea62eb147a301aff0
the349 <josephmalone25@gmail.com>
2017-05-30 22:09:15 -0400

    ver 0.0.1

--- dear.js
+++ dear.js
@@ -1,1 +1,9 @@
-
+function startdear(type) {
+  if(type == "prompt") {
+    document.write("<div id='dearjs-prompt-div' style='background-color: #C4CFCE; width: 60%; postiton: absolute; left: 0px;'>"+prompt+"</div>");
+    document.write("<div id='dearjs-prompt-div' style='background-color: #C4CFCE; width: 59%; position: relitive; top: 1%;'><input id='dearjs-prompt' type='text' style='font-family: arial; width: 20%;'></div>");
+}
+  if(type == "select") {
+    document.write("<select>"+select+"</select>");
+}
+}
